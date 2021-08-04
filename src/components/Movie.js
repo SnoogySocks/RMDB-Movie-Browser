5edd102d9e4ce25869c4315b4594fa2bff4eea3d
Felix <felixfong03@live.com>
2021-08-04 17:32:49 -0400

    added the movie info bar at the bottom of movie page :

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,26 +7,32 @@ import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 // Image
 import NoImage from "../images/no_image.jpg";
 
 const Movie = () => {
-    const { movieId } = useParams();
-    
-    const { state: movie, loading, error } = useMovieFetch(movieId);
-    
+	const { movieId } = useParams();
+
+	const { state: movie, loading, error } = useMovieFetch(movieId);
+
 	if (loading) {
-		return <Spinner />
+		return <Spinner />;
 	} else if (error) {
 		return <div>Something went wrong...</div>;
 	}
-	
+
 	return (
 		<>
-			<BreadCrumb movieTitle={movie.original_title} /> 
+			<BreadCrumb movieTitle={movie.original_title} />
 			<MovieInfo movie={movie} />
+			<MovieInfoBar
+				time={movie.runtime}
+				budget={movie.budget}
+				revenue={movie.revenue}
+			/>
 		</>
 	);
 };
